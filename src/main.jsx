c5e713fa7d60065163233ce0d215a14f8349abd8
MauricioCa07 <carrillomauricio007@gmail.com>
2024-10-01 10:43:49 -0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Secant,Gaussian } from './methods.jsx'
+
+
+
+
+createRoot(document.getElementById('root')).render(
+  <>
+  <Secant f="log(sin(x)^2 + 1) - 1/2" tolerancestr="1e-7" iterationsstr="100" x0str="0.5" x1str="1" />
+  <Gaussian matrixA="1,2,3;4,5,6;7,8,9" matrixB="1,2,3" n="3" ></Gaussian>
+  </>
+)
---
+++ src/methods.jsx
@@ -0,0 +1,84 @@
+import { StrictMode } from 'react'
+import { create, all } from 'mathjs'; 
+
+const math = create(all);
+
+export function Secant( {f,tolerancestr,iterationsstr,x0str,x1str} ){
+    const tolerance = parseFloat(tolerancestr,10)
+    const iteracions = parseInt(iterationsstr,10)
+    var X0 = parseFloat(x0str,10) 
+    var X1 = parseFloat(x1str,10)
+
+
+    if(math.evaluate(f, {x: X0}) == 0 || math.evaluate(f, {x: X1}) == 0 ){
+        return(
+            <h1> This is the root</h1>
+        );
+    }
+
+
+    const fx0 = math.evaluate(f, {x: X0}) 
+    const fx1 = math.evaluate(f, {x: X1}) 
+    var Xn = X1 - ((fx1 * (X1 - X0))/(fx1 - fx0))
+    console.log(fx0,fx1,Xn,math.evaluate(f, {x: Xn}))
+    if (isNaN(math.evaluate(f, {x: Xn}))) {
+        console.error("The function returned NaN at Xn. There might be a division by zero or another issue.");
+        return (
+            <h1>Error: The result is NaN</h1>
+        );
+    }else if (math.evaluate(f, {x: Xn}) == 0 ) {
+        return (
+            <h1>This is the root</h1>
+        );
+    }
+
+
+    var n = 0 ;
+
+
+    while(Math.abs(X1-Xn) > tolerance && math.evaluate(f, {x: Xn}) != 0 && n < iteracions){
+        console.log(X0,X1)
+        X0 = X1
+        X1 = Xn
+        console.log(X0,X1)
+        const fx0 = math.evaluate(f, {x: X0}) 
+        const fx1 = math.evaluate(f, {x: X1}) 
+        var Xn = X1 - ((fx1 * (X1 - X0))/(fx1 - fx0))
+        var fxn = math.evaluate(f, {x: Xn}) 
+        console.log(Xn,fxn)
+        if(fx1 - fxn == 0 ){
+            return (
+                <h1>Error: The result is NaN</h1>
+            );
+        }
+        n = n+1
+    }
+
+    if (Math.abs(X1-Xn) <= tolerance){
+        return (
+            <h1>root is in {Xn}</h1>
+        );
+    }
+
+    if (fxn == 0){
+        return (
+            <h1>root is in {Xn}</h1>
+        );
+    }
+
+    if (n == iteracions){
+        return (
+            <h1>rewach the toelrance</h1>
+        );
+    }
+
+}
+
+
+
+
+export function Gaussian({ matrixA, matrixB, n }) {
+    const matrixAA = matrixA.split(';').map(row => row.split(',').map(Number));
+    const matrixBB = matrixB.split(';').map(row => row.split(',').map(Number));
+    const nVar = parseInt(n,10) 
+}
