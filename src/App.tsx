5df7776fd0484f6450b0b048b4a2c7440732bdb6
MauricioCa07 <mauricioca07@kali>
2024-11-18 20:20:48 -0500

    Adding visual features

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Blog } from "./pages/Blog";
 import { Tutorials } from "./pages/Tutorials";
-import { Secant } from "./methods/1_SolucionEcuaciones/Iterativos/Secant.tsx";
+import { Secant_Main } from "./methods/1_SolucionEcuaciones/Iterativos/Secant.tsx";
 import { Bisection_Main } from "./methods/1_SolucionEcuaciones/Directos/Bisection.tsx";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/blog" element={<Blog />}></Route>
         <Route path="/tutorials" element={<Tutorials />}></Route>
-        <Route path="/Secant" element={<Secant />}></Route>
+        <Route path="/secant" element={<Secant_Main />}></Route>
         <Route path="/biseccion" element={<Bisection_Main />}></Route>
       </Routes>
     </Router>
